fa153af7687eae394106e2564924377e20df4c5f
tuhinahmed-cse <tuhinahmed.cse@gmail.com>
2022-11-08 20:28:47 +0600

    get the service data

--- index.js
+++ index.js
@@ -14,11 +14,32 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.iuz8uzh.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+
+async function run() {
+    try 
+    {
+        const serviceCollection = client.db('wedsgraphy').collection('services');
+
+
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+        
+    }
+
+    finally
+    {
+
+
+    }
+
+}
+
+run().catch(err => console.error(err));
 
 
 
